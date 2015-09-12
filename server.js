d94179f9c80719ffe41d3413135bd5086d91a0ec
Ranjit Kumar <web.ranjitkumar@gmail.com>
2015-09-12 15:56:58 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+
+var http = require('http');
+var fs = require('fs');
+var data = fs.readFileSync('demo.txt');
+var server = http.createServer(function(req, res){
+res.writeHead(200,{'Content-Type':'text/html'});
+res.write(data.toString());
+res.end('<p>program..</p><h3>End<h3>');
+});
+var port = Number(process.env.PORT || 3000)
+server.listen(port);
