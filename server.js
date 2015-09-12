4054bcbb131893eed5c3390a148923462aa4bac3
Ranjit Kumar <web.ranjitkumar@gmail.com>
2015-09-12 15:58:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 var http = require('http');
 var fs = require('fs');
-var data = fs.readFileSync('demo.txt');
+var data = fs.readFileSync('index.html');
 var server = http.createServer(function(req, res){
 res.writeHead(200,{'Content-Type':'text/html'});
 res.write(data.toString());
